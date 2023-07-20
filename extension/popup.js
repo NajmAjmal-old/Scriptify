3e88ca625c7e236b13671b08bd4089b19b3d7849
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-07-20 02:19:59 -0700

    Update popup.js

--- extension/popup.js
+++ extension/popup.js
@@ -1,12 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
   const applyButton = document.getElementById('applyButton');
+  const customCSSInput = document.getElementById('customCSS');
+  const customJSInput = document.getElementById('customJS');
+
+  // Load previously saved custom JS from local storage
+  chrome.storage.local.get(['customJS'], function (result) {
+    const savedJS = result.customJS;
+    if (savedJS) {
+      customJSInput.value = savedJS;
+    }
+  });
 
   applyButton.addEventListener('click', function () {
-    const customCSS = document.getElementById('customCSS').value;
-    const customJS = document.getElementById('customJS').value;
+    const customCSS = customCSSInput.value;
+    const customJS = customJSInput.value;
 
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.sendMessage(tabs[0].id, { css: customCSS, js: customJS });
     });
+
+    // Save custom JS to local storage
+    chrome.storage.local.set({ customJS: customJS });
   });
 });
