f3240538a1ed245da17ead5b532f33e0b920fb9f
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-07-20 02:48:52 -0700

    Update background.js

--- extension/background.js
+++ extension/background.js
@@ -1,23 +1,18 @@
+// background.js
+
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.css || message.js) {
-    chrome.scripting.executeScript({
-      target: { tabId: sender.tab.id },
-      func: injectCustomCode,
-      args: [message.css, message.js]
+  if (message.action === 'saveCustomCode') {
+    const { customCSS, customJS } = message;
+    chrome.storage.sync.set({ customCSS, customJS }, function () {
+      sendResponse({ success: true });
     });
+    return true; // Indicates that the response will be sent asynchronously
+  }
+
+  if (message.action === 'loadCustomCode') {
+    chrome.storage.sync.get(['customCSS', 'customJS'], function (result) {
+      sendResponse({ customCSS: result.customCSS, customJS: result.customJS });
+    });
+    return true; // Indicates that the response will be sent asynchronously
   }
 });
-
-function injectCustomCode(css, js) {
-  if (css) {
-    const style = document.createElement('style');
-    style.textContent = css;
-    document.head.appendChild(style);
-  }
-
-  if (js) {
-    const script = document.createElement('script');
-    script.textContent = js;
-    document.head.appendChild(script);
-  }
-}
